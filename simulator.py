7c238638e61b4b79b73a32272ce91780bf4b6b6e
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-01-17 19:51:24 -0800

    simulator initial outline

---
+++ simulator.py
@@ -0,0 +1,24 @@
+# imports
+
+# pygame initalizations
+
+# contants
+    # colors
+    # default blob values
+
+# blob class
+    # __init__
+    # search_for_food
+    # draw
+
+# main loop
+
+    # blobs list that contains list of all blobs
+        # randomly generate attribute values
+        # base it off of default blob values in constants
+
+    # while loop (game loop)
+        # randomly shuffle list each iteration
+        # iterate and perform methods for each blob during each iteration
+    
+    # use display.flip?
